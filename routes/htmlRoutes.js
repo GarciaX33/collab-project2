eadd094cf676e251337a789818e662ad2b089b7b
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-07-29 14:46:36 -0500

    fixed index.hanldebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Reservation.findAll({}).then(function(dbReservations) {
+  app.get("/", function (req, res) {
+    db.Reservation.findAll({}).then(function (dbReservations) {
       res.render("index", {
         msg: "Welcome!",
         reservations: dbReservations
@@ -12,16 +12,16 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/reservation/:id", function(req, res) {
-    db.Reservation.findOne({ where: { id: req.params.id } }).then(function(dbReservation) {
+  app.get("/reservation/:id", function (req, res) {
+    db.Reservation.findOne({ where: { id: req.params.id } }).then(function (dbReservation) {
       res.render("reservation", {
         reservation: dbReservation
       });
     });
   });
-  
+
   //Reservation
-  app.get("/reservation", function(req, res){
+  app.get("/reservation", function (req, res) {
 
     res.render("reservation")
 
@@ -37,24 +37,13 @@ module.exports = function(app) {
     res.render("aboutus")
   })
 
-  app.get("/contact", function(req, res){
-    res.render("contact")
-  })
+  // //db.Pet.findAll({where:{UserId:req.user.id}}).then(function(petData){
+  // var petData = [{ id: 1, petName: "Larry" }, { id: 2, petName: "Moe" }, { id: 3, petName: "Susie Q" }];
+  // res.render("reservation", { "pets": petData })
 
 
-  
-
-
-    //db.Pet.findAll({where:{UserId:req.user.id}}).then(function(petData){
-      var petData=[{id:1,petName:"Larry"},{id:2,petName:"Moe"},{id:3,petName:"Susie Q"}];
-      res.render("reservation",{"pets":petData})
-   // });
-    
-  });
-
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+// Render 404 page for any unmatched routes
+app.get("*", function (req, res) {
+  res.render("404");
+});
 };
