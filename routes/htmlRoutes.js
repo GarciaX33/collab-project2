999cfd2f45cc3c705ed810642d4375d7614ab39e
GarciaX33 <danny.garcia80@hotmail.com>
2019-07-29 15:05:28 -0500

    will fix doubles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,7 +30,7 @@ module.exports = function(app) {
     res.render("reservation")
 
   })
-
+  });
   //Contact Us
   app.get("/contact", function (req, res) {
     res.render("contact")
@@ -41,25 +41,22 @@ module.exports = function(app) {
     res.render("aboutus")
   })
 
-  app.get("/contact", function(req, res){
-    res.render("contact")
-  })
-
 
   
 
 
-    //db.Pet.findAll({where:{UserId:req.user.id}}).then(function(petData){
-      var petData=[{id:1,petName:"Larry"},{id:2,petName:"Moe"},{id:3,petName:"Susie Q"}];
+  //   //db.Pet.findAll({where:{UserId:req.user.id}}).then(function(petData){
+  //     var petData=[{id:1,petName:"Larry"},{id:2,petName:"Moe"},{id:3,petName:"Susie Q"}];
 
-      res.render("reservation",{"pets":petData})
-    });
+  //     res.render("reservation",{"pets":petData})
+  //   });
     
-  });
+  // });
 
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
+
 };
